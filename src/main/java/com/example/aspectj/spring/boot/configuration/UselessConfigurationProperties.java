54f5d30ff168dfcb27ecd852bba028d76a251575
Leonardo Nuzzo <leonardo.nuzzo@ifood.com.br>
2022-05-22 21:11:13 -0300

    main – Adding validation

--- src/main/java/com/example/aspectj/spring/boot/configuration/UselessConfigurationProperties.java
+++ src/main/java/com/example/aspectj/spring/boot/configuration/UselessConfigurationProperties.java
@@ -1,10 +1,15 @@
 package com.example.aspectj.spring.boot.configuration;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import javax.validation.constraints.NotBlank;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
 @ConfigurationProperties("useless.properties")
 public class UselessConfigurationProperties {
 
+    @NotBlank
     private String property = "value";
 
     public String getProperty() {
