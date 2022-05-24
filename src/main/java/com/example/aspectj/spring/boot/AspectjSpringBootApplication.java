81d8e90936469fc44bb4bcb5f17935541c87b41a
Leonardo Nuzzo <leonardo.nuzzo@ifood.com.br>
2022-05-23 21:05:55 -0300

    inner-class-bug – unused properties

--- src/main/java/com/example/aspectj/spring/boot/AspectjSpringBootApplication.java
+++ src/main/java/com/example/aspectj/spring/boot/AspectjSpringBootApplication.java
@@ -5,11 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.example.aspectj.spring.boot.aspect.JavaSyntax;
-import com.example.aspectj.spring.boot.aspect.bean.UselessBean;
-import com.example.aspectj.spring.boot.model.Child;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
